75044f00139b42b1c479a71c60d6309986986976
EliseyEkhlakov <ekhlakovea@bk.ru>
2021-01-07 01:15:41 +0400

    lesson 107

--- src/index.js
+++ src/index.js
@@ -1,44 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
-
-const reducer = (state = 0, action) => {
-  switch (action.type){
-    case 'INC': 
-      return state + 1;
-    case 'DEC':
-      return state - 1;
-    case 'RND':
-      return state + action.value;
-      case 'RST':
-        return 0;      
-    default:
-      return state;  
-  }
-}
-
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rst = () => ({type: 'RST'});
-const rnd = (value) => ({type: 'RND', value});
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './components/app';
 
 const store = createStore(reducer);
-const update = () => {
-  document.getElementById('counter').textContent = store.getState();
-}
-store.subscribe(update);
 
-document.getElementById("inc").addEventListener('click', () => {
-  store.dispatch(inc());
-});
-document.getElementById("dec").addEventListener('click', () => {
-  store.dispatch(dec());
-});
-
-document.getElementById("rst").addEventListener('click', () => {
-  store.dispatch(rst());
-});
-// document.getElementById("rnd").addEventListener('click', () => {
-//   const value = Math.floor(Math.random() * 10);
-//   store.dispatch(rnd(value));
-// });
-
+ReactDOM.render(
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    , document.getElementById('root'));
 
